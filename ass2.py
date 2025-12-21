c58b1fc946ca4a4c64066e843ee950d6aee3bcca
priyamvaday34-cell <priyamvaday34@gmail.com>
2025-12-21 17:42:52 +0530

    Update ass2.py with new code

--- ass2.py
+++ ass2.py
@@ -1,15 +1,4 @@
-#name = input("Enter your name: ")
-#print("You entered:",name)
-#print("type of input:", type(name))
-
-#age = input("Enter your age: ")
-#age_int = int(age)
-#print("You entered:",age_int)
-#print("type of input:", type(age_int))
-#height = input("Enter your height: ")
-#height_float = float(height)
-#print("You entered:",height_float)
-#print("type of input:", type(height_float))
+# 1st case
 name = input("Enter your name: ")
 print("You entered:", name)
 print("type of input:", type(name))
@@ -23,4 +12,22 @@ height = input("Enter your height: ")
 height_float = float(height)
 print("You entered:", height_float)
 print("type of input:", type(height_float))
+
+# 2nd case priya
+
+name = "Priyamvada Yadav"
+age = 20
+height = 5.6
+old = False
+a = None
+print(f"Name: {name}, type: {type(name)}")
+print(f"Age: {age}, type: {type(age)}")
+print(f"Height: {height}, type: {type(height)}")
+print(f"Old: {old}, type: {type(old)}")
+print(f"A: {a}, type: {type(a)}")
+
+
+
+
+
 
