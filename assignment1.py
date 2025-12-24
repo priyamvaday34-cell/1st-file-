1f6e827522078a459e9286416b8761c41e30e706
priyamvaday34-cell <priyamvaday34@gmail.com>
2025-12-24 17:23:52 +0530

    Added assignment1.py with code for name, college, branch & pattern printing

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#code 1:
+name = "Priyamvada Yadav"
+college = "Jaypee University"
+branch = "Computer Science"
+print(f"Name: {name}")
+print(f"College: {college}")
+print(f"Branch: {branch}")
+
+
+
+#code 2:
+for i in range(5):
+    print("* " * (i + 1))
